9d0225c847a7d986b564d133f1214368690f6231
Ankit Verma <syncing284@gmail.com>
2025-03-26 06:33:13 +0530

    removed sensitive info

--- firebase/client.ts
+++ firebase/client.ts
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBlDYoXx5cbL210f3aSCX8gKSORuQaYsZk",
-  authDomain: "interviewprep-c9294.firebaseapp.com",
-  projectId: "interviewprep-c9294",
-  storageBucket: "interviewprep-c9294.firebasestorage.app",
-  messagingSenderId: "804707257166",
-  appId: "1:804707257166:web:827bea5269ced4a9a12c1f",
-  measurementId: "G-BB0L7GFEB5"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
